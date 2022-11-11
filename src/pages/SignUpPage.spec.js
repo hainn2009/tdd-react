3ec980b1f402da8f266ca07cc690f12956d0d949
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-11-11 16:04:22 +0700

    update

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,8 +1,17 @@
-import SignUpPage from './SignUpPage'
-import { render, screen } from '@testing-library/react'
+import SignUpPage from "./SignUpPage";
+import { render, screen } from "@testing-library/react";
 
-it("has header", () => {
-    render(<SignUpPage />);
-    const header = screen.queryByRole("heading", { name: "Sign Up" })
-    expect(header).toBeInTheDocument();
-})
+describe("Signup Page", () => {
+    describe("Layout", () => {
+        it("has header", () => {
+            render(<SignUpPage />);
+            const header = screen.queryByRole("heading", { name: "Sign Up" });
+            expect(header).toBeInTheDocument();
+        });
+        it("has username input", () => {
+            const { container } = render(<SignUpPage />);
+            const input = container.querySelector("input");
+            expect(input).toBeInTheDocument();
+        });
+    });
+});
