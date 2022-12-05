ecbcf8e7bef3f8392d102c4510e5b9136dfee94b
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-12-05 19:54:21 +0700

    Add new language

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as apiCalls from "./api/apiCall";
 
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        // the translations
+        // (tip move them in a JSON file and import them,
+        // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+        resources: {
+            en: {
+                translation: {
+                    signUp: "Sign Up",
+                },
+            },
+        },
+        lng: "en", // if you're using a language detector, do not define the lng option
+        fallbackLng: "en",
+
+        interpolation: {
+            escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        },
+    });
+
 const actions = {
     postSignup: apiCalls.Signup,
 };
