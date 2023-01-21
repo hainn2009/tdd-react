fa5e4e87d5e0aaa887b5d60b572622782a8d69ac
Nguyen Ngoc Hai <hainn2009@gmail.com>
2023-01-21 18:26:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LanguageSelector from "./components/LanguageSelector";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import logo from "./assets/logo.jpg";
-import { BrowserRouter, Route, Link, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Link, createBrowserRouter, Outlet } from "react-router-dom";
 
 function App() {
     const { t } = useTranslation();
@@ -18,45 +18,48 @@ function App() {
         // setPath(path);
     };
     return (
-        <h1>Hello World</h1>
+        // <h1>Hello World</h1>
         // <BrowserRouter>
-        //     <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
-        //         <div className="container">
-        //             {/* <a className="navbar-brand" href="/" title="Home" onClick={handleNavLinkClick}>
-        //                 <img src={logo} alt="My TDD project" width="60" />
-        //                 My TDD project
-        //             </a> */}
-        //             <Link className="navbar-brand" to="/" title="Home">
-        //                 <img src={logo} alt="My TDD project" width="60" />
-        //                 My TDD project
-        //             </Link>
-        //             <div className="navbar-nav">
-        //                 {/* <a className="nav-link" href="/signup" title="Signup" onClick={handleNavLinkClick}>
-        //                     {t("signUp")}
-        //                 </a>
-        //                 <a className="nav-link" href="/login" title="Login" onClick={handleNavLinkClick}>
-        //                     Login
-        //                 </a> */}
-        //                 <Link className="nav-link" href="/signup" title="Signup" onClick={handleNavLinkClick}>
-        //                     {t("signUp")}
-        //                 </Link>
-        //                 <Link className="nav-link" href="/login" title="Login" onClick={handleNavLinkClick}>
-        //                     Login
-        //                 </Link>
-        //             </div>
-        //         </div>
-        //     </nav>
-        //     <div className="container">
-        //         <Route exact path="/" component={HomePage} />
-        //         <Route path="/signup" component={SignUpPage} />
-        //         <Route path="/login" component={LoginPage} />
-        //         <Route path="/user/:id" component={UserPage} />
-        //         {/* {path === "/" && <HomePage />}
-        //         {path === "/signup" && <SignUpPage />}
-        //         {path === "/login" && <LoginPage />}
-        //         {path.startsWith("/user/") && <UserPage />} */}
-        //         <LanguageSelector />
-        //     </div>
+        <>
+            <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+                <div className="container">
+                    {/* <a className="navbar-brand" href="/" title="Home" onClick={handleNavLinkClick}>
+                        <img src={logo} alt="My TDD project" width="60" />
+                        My TDD project
+                    </a> */}
+                    <Link className="navbar-brand" to="/" title="Home">
+                        <img src={logo} alt="My TDD project" width="60" />
+                        My TDD project
+                    </Link>
+                    <div className="navbar-nav">
+                        {/* <a className="nav-link" href="/signup" title="Signup" onClick={handleNavLinkClick}>
+                            {t("signUp")}
+                        </a>
+                        <a className="nav-link" href="/login" title="Login" onClick={handleNavLinkClick}>
+                            Login
+                        </a> */}
+                        <Link className="nav-link" href="/signup" title="Signup" onClick={handleNavLinkClick}>
+                            {t("signUp")}
+                        </Link>
+                        <Link className="nav-link" href="/login" title="Login" onClick={handleNavLinkClick}>
+                            Login
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+            <div className="container">
+                <Outlet />
+                {/* <Route exact path="/" component={HomePage} /> */}
+                {/* <Route path="/signup" component={SignUpPage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/user/:id" component={UserPage} /> */}
+                {/* {path === "/" && <HomePage />}
+                {path === "/signup" && <SignUpPage />}
+                {path === "/login" && <LoginPage />}
+                {path.startsWith("/user/") && <UserPage />} */}
+                <LanguageSelector />
+            </div>
+        </>
         // </BrowserRouter>
     );
 }
--- src/index.js
+++ src/index.js
@@ -7,16 +7,28 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as apiCalls from "./api/apiCall";
 import "./locale/i18n";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 // const actions = {
 //     postSignup: apiCalls.Signup,
 // };
 
-const router = createBrowserRouter([{
-    path: "/",
-    element: <App />
-}])
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+            <Route path="/signup" component={SignUpPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/user/:id" component={UserPage} />
+        </Route>
+
+    )
+    //     [{
+    //     path: "/",
+    //     element: <App />
+    // }]
+)
 
 ReactDOM.render(
     <React.StrictMode>
