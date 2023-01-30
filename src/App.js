1d1ca3fb42d932f18e0dc2c8897db406a3876310
Nguyen Ngoc Hai <hainn2009@gmail.com>
2023-01-30 09:33:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
         event.preventDefault();
         const path = event.currentTarget.attributes.href.value;
         window.history.pushState({}, "", path);
-        // setPath(path);
+        setPath(path);
     };
     return (
         // <h1>Hello World</h1>
